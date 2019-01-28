d9d3238a13b2687685f2979192c54725d0a3faa0
Ivà Domingo <iva@iva.cat>
2019-01-28 22:05:23 +0100

    working

--- js/main.js
+++ js/main.js
@@ -51,7 +51,8 @@ var pintarMenu = function(nom, data){
       element.classList.add('dropdown-item');
       let name = e.name;
       element.innerHTML = name.toUpperCase().charAt(0)+name.substring(1,name.length);
-      element.onclick = pintarImatges(name);
+      element.addEventListener("click",  pintarImatges(name));
+     //element.onclick = pintarImatges(name);
       menu.appendChild(element);      
     });
   }
